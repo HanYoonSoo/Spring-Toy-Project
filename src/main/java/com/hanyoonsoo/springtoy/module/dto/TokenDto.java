13458b61397ecef20b65817bcc4c9a03d5bae412
HanYoonSoo <hdbstn3055@gmail.com>
2024-02-04 22:42:27 +0900

    feat: JwtToken Set

---
+++ src/main/java/com/hanyoonsoo/springtoy/module/dto/TokenDto.java
@@ -0,0 +1,21 @@
+package com.hanyoonsoo.springtoy.module.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 클라이언트에 토큰을 보내기 위한 DTO
+ * grantType은 JWT에 대한 인증 타입이다.
+ */
+@Builder
+@Data
+@AllArgsConstructor
+public class TokenDto {
+
+    private String grantType; // JWT에 대한 인증 타입. Bearer를 사용
+    private final String authorizationType;
+    private String accessToken;
+    private String refreshToken;
+    private final Long accessTokenExpiresIn;
+}
---
+++ src/main/java/com/hanyoonsoo/springtoy/module/global/security/JwtTokenProvider.java
@@ -0,0 +1,173 @@
+package com.hanyoonsoo.springtoy.module.global.security;
+
+import com.hanyoonsoo.springtoy.module.dto.TokenDto;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * JWT 토큰을 생성, 토큰 복호화 및 정보 추출, 토큰 유효성 검증 클래스
+ * jwt.secret는 토큰의 암호화, 복호화를 위한 secret key로 HS256 알고리즘 사용을 위해, 256비트보다 커야함.
+ * 한 단어에 1바이트이므로, 32글자 이상 설정.
+ */
+@Component
+@Slf4j
+public class JwtTokenProvider {
+
+    public static final String BEARER = "Bearer";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String REFRESH_HEADER = "Refresh";
+    public static final String BEARER_PREFIX = "Bearer ";
+    private static final String AUTHORITIES_KEY = "auth";
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.accessToken-validity-in-seconds}")
+    private long accessTokenValidityInSeconds;
+
+    @Value("${jwt.refreshToken-validity-in-seconds}")
+    private long refreshTokenValidityInSeconds;
+
+    private Key key;
+
+    @PostConstruct
+    public void keyInit(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.accessTokenValidityInSeconds *= 1000;
+        this.refreshTokenValidityInSeconds *= 1000;
+    }
+
+    // 유저 정보를 토대로 AccessToken, RefreshToken 생성
+    public TokenDto generateToken(CustomUserDetails customUserDetails){
+        // 권한 가져오기
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(AUTHORITIES_KEY, customUserDetails.getRole());
+
+        long now = (new Date()).getTime();
+        Date accessValidity = new Date(now + this.accessTokenValidityInSeconds);
+        Date refreshValidity = new Date(now + this.refreshTokenValidityInSeconds);
+
+        // AccessToken 생성
+        String accessToken = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(customUserDetails.getEmail())
+                .setExpiration(accessValidity)
+                .setIssuedAt(Calendar.getInstance().getTime())
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        // RefreshToken 생성
+        String refreshToken = Jwts.builder()
+                .setSubject(customUserDetails.getEmail())
+                .setIssuedAt(Calendar.getInstance().getTime())
+                .setExpiration(refreshValidity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return TokenDto.builder()
+                .grantType(BEARER)
+                .authorizationType(AUTHORIZATION_HEADER)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .accessTokenExpiresIn(accessTokenValidityInSeconds)
+                .build();
+    }
+
+    // JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼냄
+    public Authentication getAuthentication(String accessToken){
+        // 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if(claims.get(AUTHORITIES_KEY) == null){
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        String authority = claims.get(AUTHORITIES_KEY).toString();
+
+        CustomUserDetails customUserDetails = CustomUserDetails.of(
+                claims.getSubject(), authority);
+
+        log.info("# AuthUser.getRoles 권한 체크 = {}", customUserDetails.getAuthorities().toString());
+
+        return new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+    }
+
+    // 토큰 정보 검증
+    public boolean validateToken(String token, HttpServletResponse response){
+        try{
+            parseClaims(token);
+        } catch(MalformedJwtException e){
+            log.info("Invalid JWT token");
+            log.trace("Invalid JWT token trace = {}", e);
+        } catch (ExpiredJwtException e){
+            log.info("Expired JWT token");
+            log.trace("Expired JWT token trace = {}", e);
+        } catch (UnsupportedJwtException e){
+            log.info("Unsupported JWT token");
+            log.trace("Unsupported JWT token trace = {}", e);
+        } catch(IllegalArgumentException e){
+            log.info("JWT claims string is empty");
+            log.trace("JWT claims string is empty trace = {}", e);
+        }
+
+        return true;
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public void accessTokenSetHeader(String accessToken, HttpServletResponse response){
+        String headerValue = BEARER_PREFIX + accessToken;
+        response.setHeader(AUTHORIZATION_HEADER, headerValue);
+    }
+
+    public void refreshTokenSetHeader(String refreshToken, HttpServletResponse response){
+        response.setHeader("Refresh", refreshToken);
+    }
+
+    // Request Header의 Access Token 정보 추출
+    public String resolveAccessToken(HttpServletRequest request){
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)){
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+    // Request Header에 Refresh Token 정보 추출
+    public String resolveRefreshToken(HttpServletRequest request){
+        String bearerToken = request.getHeader(REFRESH_HEADER);
+        if(StringUtils.hasText(bearerToken)){
+            return bearerToken;
+        }
+
+        return null;
+    }
+}
